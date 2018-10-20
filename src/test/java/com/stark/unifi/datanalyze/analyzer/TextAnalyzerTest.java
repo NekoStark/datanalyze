2a948881c46c2edca0bda93c6c2c1168bb7eb557
Simone Mattolini <simone.mattolini@jaewa.com>
2018-10-20 13:35:22 +0200

    improved testing with mocking

--- src/test/java/com/stark/unifi/datanalyze/analyzer/TextAnalyzerTest.java
+++ src/test/java/com/stark/unifi/datanalyze/analyzer/TextAnalyzerTest.java
@@ -17,7 +17,11 @@ public class TextAnalyzerTest {
 		String text = new FileContentReader().read("/txt/basic.txt");
 		Document result = analyzer.analyzeText(text);
 		
+		assertEquals(text, result.getOriginalText());
+		
+		assertEquals(6, result.getSentences().size());
 		assertEquals(6, result.getSentenceCount());
+		
 		assertEquals(36, result.getWordCount());
 		assertEquals(205, result.getCharacterCount());
 		assertEquals(137, result.getSyllableCount());
